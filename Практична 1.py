e41381c5e915ef89d936cc382c3f6eaec196cf88
Vlad Tkachuk <vladtkachuk3223@gmail.com>
2024-09-13 23:00:55 +0300

    Add files via upload

---
+++ Практична 1.py
@@ -0,0 +1,9 @@
+HW = "Hello World"
+name = "Vlad"
+lastName = "Tkachuk"
+age =  16
+print(f"{HW}\nname: {name}\nlast name: {lastName}\nage: {age}")
+print(f"Type's:\nHello World: {type(HW)} \nName: {type(name)} \nlast name: {type(lastName)} \nage: {type(age)}")
+
+
+
